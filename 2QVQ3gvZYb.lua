e718d7c106e92d36cfec8a7d8e4fb539e08b25c8
cVHjBuQuMZdoGl <doitenroirac2@gmail.com>
2026-06-22 06:07:31 +0700

    upload 2QVQ3gvZYb.lua

---
+++ 2QVQ3gvZYb.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/atWCX4kh"))()
+
+Execute after ue loads, reload it each match. make sure to use the config you purchased with this or our other config on the market.
