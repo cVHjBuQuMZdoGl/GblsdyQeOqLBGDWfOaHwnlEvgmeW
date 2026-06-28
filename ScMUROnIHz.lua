1dab35497ae3f513fc83a03c83c85b54e7a85c8f
cVHjBuQuMZdoGl <doitenroirac2@gmail.com>
2026-06-28 10:12:24 +0700

    upload ScMUROnIHz.lua

---
+++ ScMUROnIHz.lua
@@ -0,0 +1,10 @@
+--// Services
+local Players = game:GetService("Players")
+
+--//  Main Script Content
+local MainScript = [[
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dreadheadscripts/ScriptHub/refs/heads/main/Aimbot-Script/Base-Gui.lua"))()
+]]
+
+--// Execution
+loadstring(MainScript)()
